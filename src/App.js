a1f29ddefe30a70d351067dd72990bec529a8fda
gelbwong <karinawong04@gmail.com>
2024-06-26 15:47:59 +1000

    Removed Unnecessary Files & Added Bootstrap (CDN)
    -Barebones of a react app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather React App</h1>
+        <p>This do be a Site about the forecast and current weather</p>
       </header>
     </div>
   );
