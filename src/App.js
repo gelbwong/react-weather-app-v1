c58ecb6251c84c14e303eb0c87ad83e9cef50e87
gelbwong <karinawong04@gmail.com>
2024-06-26 23:41:09 +1000

    Static HTML/CSS Planning
    - Establishing theme (buddhist talisman)
    -Building and styling aesthetics and general function
    -Concept is a weather app that tells you the time
    -Added nice sound to the background via svg

--- src/App.js
+++ src/App.js
@@ -1,35 +1,74 @@
 import "./App.css";
 import Footer from "./Footer";
+import Design from "./images/sample-designs.png";
+import LeftDesign from "./images/Left-design.png";
+import RightDesign from "./images/Right-design.png";
 
 function App() {
   return (
     <div className="App container">
-      <header className="App-header">
-        <h1>Melbourne</h1>
-        <h2>Australia</h2>
+      <div className="row">
+        <div className="col-4">
+          <img
+            src={LeftDesign}
+            alt="Handrawn design for left of the title of page."
+            className="img-fluid"
+          />
+        </div>
+        <div className="col-4">
+          <h1 className="city-title">Melbourne</h1>
+          <h2 className="country-title">Australia</h2>
+        </div>
+        <div className="col-4">
+          <img
+            src={RightDesign}
+            alt="Handrawn design for right of the title of page."
+            className="img-fluid"
+          />
+        </div>
+      </div>
+
+      <form className="search-engine">
         <div className="row">
-          <div className="col-4">
-            <img
-              src="src/images/sample-designs.png"
-              alt="Simple mirrorred freehand design"
+          <div className="col-10">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control search-bar"
             />
           </div>
-          <div className="col-4">
-            <ul>
-              <li>Temp: 5°C</li>
-              <li>Humidity: 5°C</li>
-              <li>Wind: 5°C</li>
-              <li>Percipitation: 5°C</li>
-            </ul>
-          </div>
-          <div className="col-4">
-            <img
-              src="src/images/sample-designs.png"
-              alt="Simple mirrorred freehand design"
-            />
+          <div className="col-2">
+            <input type="submit" value="Search" className="btn search-button" />
           </div>
         </div>
-      </header>
+      </form>
+
+      <div className="row current-weather-details">
+        <div className="col-4">
+          <img
+            src={Design}
+            alt="Simple mirrorred freehand design"
+            className="img-fluid"
+          />
+        </div>
+        <div className="col-4">
+          <ul className="list-group list-group-flush current-weather">
+            <li className="list-group-item">Temp: 5°C</li>
+            <li className="list-group-item">Humidity: 5°C</li>
+            <li className="list-group-item">Wind: 5°C</li>
+            <li className="list-group-item">Percipitation: 5°C</li>
+            <li className="list-group-item">Percipitation: 5°C</li>
+          </ul>
+        </div>
+        <div className="col-4">
+          <img
+            src={Design}
+            alt="Simple mirrorred freehand design"
+            className="img-fluid"
+          />
+        </div>
+      </div>
+
       <Footer />
     </div>
   );
